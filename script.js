591e708c443d4b4905044653f8648a6ce24c663c
bhargavi2520 <160613394+bhargavi2520@users.noreply.github.com>
2025-10-05 17:15:41 +0530

    made contact form functional

--- script.js
+++ script.js
@@ -60,3 +60,27 @@ if (savedTheme === "dark") {
   heroContent.classList.remove("text-white");
   updateHeroVideoSource(false);
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const contactForm = document.getElementById('contact-form');
+    const contactMessage = document.getElementById('contact-message');
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_wlhpkv9', 'template_hjj0sif', contactForm, 'FbyQzk1g_nN2pwbIB')
+            .then(() => {
+                contactMessage.textContent = 'Message sent successfully!';
+                setTimeout(() => {
+                    contactMessage.textContent = '';
+                }, 5000);
+                contactForm.reset();
+            })
+            .catch((error) => {
+                console.error('Error sending email:', error);
+                contactMessage.textContent = `Message not sent (Error: ${error.text})`;
+            });
+    };
+
+    contactForm.addEventListener('submit', sendEmail);
+});
